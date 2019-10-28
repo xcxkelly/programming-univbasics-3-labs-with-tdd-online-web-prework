b86227032df23593333d224ad44337303bb7c3c2
Kelly Small <xcxkelly98@gmail.com>
2019-10-28 08:39:48 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,31 @@
-# Add your variables here
+
+puts "enter a number:
+num1 = gets.chomp()"
+puts "enter another number:
+num2 = gets.chomp()"
+
+puts (num1 + num2)
+
+puts "enter a number:"
+num1 = gets.chomp()
+puts "enter another number:"
+num2 = gets.chomp()"
+
+puts (num1 - num2)
+
+puts "enter a number:"
+num1 = gets.chomp()
+puts "enter another number:"
+num2 = gets.chom()"
+
+puts (num1 / num2)
+
+puts "enter a number:"
+num1 = gets.chomp()
+puts "enter another number:"
+num2 = gets.chomp()"
+
+puts (num1 * num2)
+
+ 
+
