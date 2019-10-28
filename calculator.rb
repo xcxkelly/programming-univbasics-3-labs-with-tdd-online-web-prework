fbbd660307d46ec6c54daf524a8d4ca0d042b9f6
Kelly Small <xcxkelly98@gmail.com>
2019-10-28 08:29:52 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -6,12 +6,26 @@ num2 = gets.chomp()"
 
 puts (num1 + num2)
 
-puts "enter a number:
+puts "enter a number:"
 num1 = gets.chomp()
 puts "enter another number:"
-num2 = gets.chomp()
+num2 = gets.chomp()"
 
 puts (num1 - num2)
- filter_parameter_logging
+
+puts "enter a number:"
+num1 = gets.chomp()
+puts "enter another number:"
+num2 = gets.chom()"
+
+puts (num1 / num2)
+
+puts "enter a number:"
+num1 = gets.chomp()
+puts "enter another number:"
+num2 = gets.chomp()"
+
+puts (num1 * num2)
+
  
 
